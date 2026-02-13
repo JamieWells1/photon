8fc6096a5e0420fe0ac2279d3e526d692e9ff864
Jamie Wells <jamiewells528@gmail.com>
2026-02-13 14:14:34 +0000

    setup

--- photon.c
+++ photon.c
@@ -1,11 +1,11 @@
+#include <blink.pio.h>
+#include <hardware/pio.h>
+#include <pico/cyw43_arch.h>
+#include <pico/stdlib.h>
 #include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/pio.h"
-#include "pico/cyw43_arch.h"
 
-#include "blink.pio.h"
-
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
+void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq)
+{
     blink_program_init(pio, sm, offset, pin);
     pio_sm_set_enabled(pio, sm, true);
 
@@ -16,15 +16,13 @@ void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
     pio->txf[sm] = (125000000 / (2 * freq)) - 3;
 }
 
-
-
-
 int main()
 {
     stdio_init_all();
 
     // Initialise the Wi-Fi chip
-    if (cyw43_arch_init()) {
+    if (cyw43_arch_init())
+    {
         printf("Wi-Fi init failed\n");
         return -1;
     }
@@ -33,29 +31,37 @@ int main()
     PIO pio = pio0;
     uint offset = pio_add_program(pio, &blink_program);
     printf("Loaded program at %d\n", offset);
-    
-    #ifdef PICO_DEFAULT_LED_PIN
+
+#ifdef PICO_DEFAULT_LED_PIN
+    printf("Using PICO_DEFAULT_LED_PIN: %d\n", PICO_DEFAULT_LED_PIN);
     blink_pin_forever(pio, 0, offset, PICO_DEFAULT_LED_PIN, 3);
-    #else
+#else
+    printf("PICO_DEFAULT_LED_PIN not defined, using pin 6\n");
     blink_pin_forever(pio, 0, offset, 6, 3);
-    #endif
+#endif
     // For more pio examples see https://github.com/raspberrypi/pico-examples/tree/master/pio
 
     // Enable wifi station
     cyw43_arch_enable_sta_mode();
 
     printf("Connecting to Wi-Fi...\n");
-    if (cyw43_arch_wifi_connect_timeout_ms("Your Wi-Fi SSID", "Your Wi-Fi Password", CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+    if (cyw43_arch_wifi_connect_timeout_ms("Your Wi-Fi SSID", "Your Wi-Fi Password",
+                                           CYW43_AUTH_WPA2_AES_PSK, 30000))
+    {
         printf("failed to connect.\n");
         return 1;
-    } else {
+    }
+    else
+    {
         printf("Connected.\n");
         // Read the ip address in a human readable way
-        uint8_t *ip_address = (uint8_t*)&(cyw43_state.netif[0].ip_addr.addr);
-        printf("IP address %d.%d.%d.%d\n", ip_address[0], ip_address[1], ip_address[2], ip_address[3]);
+        uint8_t *ip_address = (uint8_t *)&(cyw43_state.netif[0].ip_addr.addr);
+        printf("IP address %d.%d.%d.%d\n", ip_address[0], ip_address[1], ip_address[2],
+               ip_address[3]);
     }
 
-    while (true) {
+    while (true)
+    {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
