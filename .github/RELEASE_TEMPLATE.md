e9d434282e7d2eb4c9dd13a0a18d527105c1f291
Jamie Wells <jamiewells528@gmail.com>
2026-02-14 21:33:15 +0000

    add .github and test setup

---
+++ .github/RELEASE_TEMPLATE.md
@@ -0,0 +1,48 @@
+# **Photon {{VERSION}} – {{SHORT_TITLE}}**
+
+> 🤖 AI-generated release notes based on code changes and commit messages
+
+---
+
+## 📦 Download
+
+**Firmware:** [photon.uf2](../../releases/download/{{VERSION}}/photon.uf2)
+
+---
+
+## 📝 Summary
+
+{{SUMMARY}}
+
+---
+
+## ✨ What's New
+
+#### {{FEATURE_GROUP}}
+
+- {{DETAILS}}
+
+---
+
+## 🚀 Enhancements
+
+- {{ENHANCEMENTS}}
+
+---
+
+## 🐛 Bug Fixes
+
+- {{BUG_DESCRIPTIONS}}
+
+---
+
+## 📚 Documentation Updates
+
+- {{DOC_UPDATES}}
+
+---
+
+## 💡 Need Help?
+
+- [Report an issue](../../issues)
+- [View documentation](../../blob/main/README.md)
---
+++ build-tests/CMakeFiles/photon.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/photon.dir/main.c.o"
+  "CMakeFiles/photon.dir/main.c.o.d"
+  "CMakeFiles/photon.dir/src/common/const.c.o"
+  "CMakeFiles/photon.dir/src/common/const.c.o.d"
+  "CMakeFiles/photon.dir/src/display/graphics.c.o"
+  "CMakeFiles/photon.dir/src/display/graphics.c.o.d"
+  "CMakeFiles/photon.dir/src/display/led.c.o"
+  "CMakeFiles/photon.dir/src/display/led.c.o.d"
+  "CMakeFiles/photon.dir/src/input/input.c.o"
+  "CMakeFiles/photon.dir/src/input/input.c.o.d"
+  "CMakeFiles/photon.dir/src/menu.c.o"
+  "CMakeFiles/photon.dir/src/menu.c.o.d"
+  "CMakeFiles/photon.dir/src/middleware/http.c.o"
+  "CMakeFiles/photon.dir/src/middleware/http.c.o.d"
+  "CMakeFiles/photon.dir/src/middleware/wifi.c.o"
+  "CMakeFiles/photon.dir/src/middleware/wifi.c.o.d"
+  "CMakeFiles/photon.dir/src/modes/games/tetris.c.o"
+  "CMakeFiles/photon.dir/src/modes/games/tetris.c.o.d"
+  "CMakeFiles/photon.dir/src/modes/stocks.c.o"
+  "CMakeFiles/photon.dir/src/modes/stocks.c.o.d"
+  "photon.bin"
+  "photon.dis"
+  "photon.elf"
+  "photon.hex"
+  "photon.pdb"
+  "photon.uf2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/photon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
