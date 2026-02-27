b4d4dbf67b19155f3fbbc4b36168a3e44f893d8d
Jamie Wells <jamiewells528@gmail.com>
2026-02-27 15:01:51 +0000

    Add location

--- src/common/const.h
+++ src/common/const.h
@@ -40,45 +40,4 @@ extern const DebugLevel DEBUG_LEVEL;
 // Weather
 #define WEATHER_HOURS 48
 
-// Locations + timezones
-#define LOCATION LOC_TOKYO
-
-#define LAT_LONDON 51.6611
-#define LAT_MIAMI 25.7617
-#define LAT_LOS_ANGELES 34.0522
-#define LAT_CHICAGO 41.8781
-#define LAT_TOKYO 35.6762
-#define LAT_SYDNEY -33.8688
-#define LAT_STOCKHOLM 59.3293
-#define LAT_RIO_DE_JANEIRO -22.9068
-#define LAT_CAPE_TOWN -33.9249
-#define LAT_ATHENS 37.9838
-#define LAT_DEFAULT 0
-
-#define LON_LONDON 0.3970
-#define LON_MIAMI -80.1918
-#define LON_LOS_ANGELES -118.2437
-#define LON_CHICAGO -87.6298
-#define LON_TOKYO 139.6503
-#define LON_SYDNEY 151.2093
-#define LON_STOCKHOLM 18.0686
-#define LON_RIO_DE_JANEIRO -43.1729
-#define LON_CAPE_TOWN 18.4241
-#define LON_ATHENS 23.7275
-#define LON_DEFAULT 0
-
-typedef enum
-{
-    LOC_LONDON,
-    LOC_MIAMI,
-    LOC_LOS_ANGELES,
-    LOC_CHICAGO,
-    LOC_TOKYO,
-    LOC_SYDNEY,
-    LOC_STOCKHOLM,
-    LOC_RIO_DE_JANEIRO,
-    LOC_CAPE_TOWN,
-    LOC_ATHENS,
-} Location;
-
 #endif  // CONST_H
